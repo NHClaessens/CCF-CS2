606476e279caa8068f18413bec4b756ab643bc1d
sirnathan000 <135742667+sirnathan000@users.noreply.github.com>
2025-03-17 15:41:37 +0100

    Update parser_log.py
    now with propper error handeling

--- parser_log.py
+++ parser_log.py
@@ -7,11 +7,9 @@ import re
 def get_files(dir_src, dir_dest):
 	relevant_log = []
 	files = os.listdir(dir_src)
-#	print(files)
 	for file in files:
 		if "001.log" in file:
 			 relevant_log.append(file)
-#	print(relevant_log)
 	for file in relevant_log:
 		file = dir_src + "/" + file
 		extract_chat(file, dir_dest)
@@ -58,11 +56,12 @@ def write_to_file(file, line):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='cs2 logs parser for entire folder')
     parser.add_argument('--srcdir', type=util.dir_path, help='Path to the directory containing .log files')
-    parser.add_argument('--dstdir', type=util.dir_path, help='Path to the directory for output')
+    parser.add_argument('--dstdir', type=util.dir_path, required=True, help='Path to the directory for output')
     parser.add_argument('--srcfile', type=util.file_path, help='path to soruce file')
     args = parser.parse_args()
     if args.srcdir and args.dstdir:
         get_files(args.srcdir, args.dstdir)
-    if args.srcfile and args.dstdir:
+    elif args.srcfile and args.dstdir:
         extract_chat(args.srcfile, args.dstdir)
-
+    else:
+        raise argparse.ArgumentTypeError(f"need either a source file or a source directory")
